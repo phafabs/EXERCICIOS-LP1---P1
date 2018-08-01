dd8ee47c496fb2fc5219266fc4620f0bd493a604
phafabs <40701514+phafabs@users.noreply.github.com>
2018-08-01 11:12:35 -0600

    Create Matrículas Duplas.py

---
+++ Matrículas Duplas.py
@@ -0,0 +1,15 @@
+p2 = []
+p3 = []
+duplo = []
+a = 0
+b = 0
+for a in range(1,5):
+    a = int(input('Digite algo'))
+    p2.append(a)
+for b in range(1,4):
+    b = int(input('algo'))
+    p3.append(b)
+for a in p2:
+  if a in p3:
+    duplo.append(a)
+print(duplo)  
