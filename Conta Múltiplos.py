f0181c07d297dd896bee78db8d4aeeba39a994b0
phafabs <40701514+phafabs@users.noreply.github.com>
2018-08-01 20:42:02 -0300

    Create Conta Múltiplos.py

---
+++ Conta Múltiplos.py
@@ -0,0 +1,12 @@
+a = int(input(''))
+b = int(input(''))
+i = 1
+cont = 0
+cont2 = 0
+for i in range(1, 50 + 1):
+  if a % i == 0:
+        cont = cont + 1
+  if b % i == 0:
+        cont2 = cont2 + 1
+  i = i + 1
+print(cont + cont2)
