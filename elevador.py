fe0ea45d5e029db6f1a004f393f64413e4960bc0
phafabs <40701514+phafabs@users.noreply.github.com>
2018-08-04 16:39:13 -0600

    Update elevador.py

--- elevador.py
+++ elevador.py
@@ -5,8 +5,8 @@ max_pessoas = 7
 while (pessoa <= 7) and (peso_total <= peso_limite):	
 	print("Passageiro: ",pessoa)
 	peso_pessoa = float(input("Digite o peso do passageiro: "))
-	peso_total += peso_pessoa
-	pessoa += 1
+	peso_total = peso_total + peso_pessoa
+	pessoa = pessoa + 1
 if (pessoa >= max_pessoas):
 	print("Atingiu o limite máximo de pessoas!", pessoa - 1)
 elif (pessoa < max_pessoas) and (peso_total >= peso_limite):
