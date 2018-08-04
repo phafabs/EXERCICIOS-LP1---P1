ae2b24fafe47757eea9c1f74faec044b554b9b59
phafabs <40701514+phafabs@users.noreply.github.com>
2018-08-04 11:05:41 -0300

    Update Média dos elementos pares.py

--- Média dos elementos pares.py
+++ Média dos elementos pares.py
@@ -2,15 +2,11 @@ a = input("Digite um numero: ")
 a = a.split()
 lista = []
 par = []
-
 lista = [int(i) for i in a]
-
 for i in lista:
     if i % 2 == 0 and i >= 0:
         par.append(i)
 if len(par) == 0:
-    print("-1")
-    
+    print("-1")    
 resultado = sum(par)/len(par)
-
 print('{:.2f}'.format(resultado))
