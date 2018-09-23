c45d981317d8087f2ddd7cfd1f484bb208550725
phafabs <40701514+phafabs@users.noreply.github.com>
2018-09-23 08:43:06 -0300

    Delete Sequencia de inteiros_pares.py

--- Sequencia de inteiros_pares.py
+++
@@ -1,9 +0,0 @@
-def  inteiro ( n ):
-    pos =  0
-    enquanto pos <= n:
-        se pos ==  0 :
-            imprimir (pos)
-        elif pos %  2  ==  0 :
-            imprimir (pos)
-        pos + =  1
-inteiro ( 9 )
