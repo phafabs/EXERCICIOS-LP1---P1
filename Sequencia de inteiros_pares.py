d1d30bf403a97e21493dc03f45e55e139972fa6e
phafabs <40701514+phafabs@users.noreply.github.com>
2018-09-23 08:32:54 -0300

    Create Sequencia de inteiros_pares.py

---
+++ Sequencia de inteiros_pares.py
@@ -0,0 +1,9 @@
+def  inteiro ( n ):
+    pos =  0
+    enquanto pos <= n:
+        se pos ==  0 :
+            imprimir (pos)
+        elif pos %  2  ==  0 :
+            imprimir (pos)
+        pos + =  1
+inteiro ( 9 )
